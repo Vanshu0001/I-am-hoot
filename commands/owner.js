ccf5b37bffae6eba7def7cdbdc478e971758c0c2
Vanshu0001 <156429013+Vanshu0001@users.noreply.github.com>
2024-01-28 17:10:14 +0530

    owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -17,24 +17,24 @@ const { ApplicationCommandOptionType } = require('discord.js');
 const db = require("../mongoDB");
 
 module.exports = {
-  name: "owner",
+  name: "Imu",
   description: "Get information about bot owner.",
   permissions: "0x0000000000000800",
   options: [],
 
   run: async (client, interaction) => {
     try {
-      const youtubeLink = 'https://discord.gg/FUEHs7RCqz';
-      const InstagramLink = 'https://www.instagram.com/rtxxgg/';
+      const youtubeLink = 'youtube.com';
+      const InstagramLink = 'instagram.com';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
             .setColor('#da2a41')
             .setAuthor({
           name: 'Owner',
-          iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          iconURL: 'https://cdn.discordapp.com/attachments/1190992843779604543/1201128205311033474/download.jpeg?ex=65c8b0d0&is=65b63bd0&hm=b5dffafd93c3883227d9a22349852828d103f619e568f4eb85175668daa1b60c&',
+          url: 'https://discord.gg/tTUa9VnWWn'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka RTX. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n ▶️ Myself Vansh aka Imu. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [TonyStarkPlayz](${youtubeLink})\n Instagram : 💙 [Rn not available](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
